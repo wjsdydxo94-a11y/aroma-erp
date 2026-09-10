3b479cbfd17312acbbee4aef6f79431ae6f61915
wjsdydxo94-a11y <wjsdydxo94@gmail.com>
2026-09-10 13:39:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import csv
 import io
 import pandas as pd
 from fastapi import FastAPI, HTTPException, Response, File, UploadFile, Query
+from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 from database import engine, Base, SessionLocal, MaterialMaster
 
