60f9a5db6f1fdede39e62068c68d8d00e4e0373f
wjsdydxo94-a11y <wjsdydxo94@gmail.com>
2026-09-11 17:14:14 +0900

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-# SQLite 데이터베이스 엔진 및 세션 설정
 engine = create_engine('sqlite:///erp_factory.db', connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
